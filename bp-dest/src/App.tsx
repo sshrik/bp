9d0b8155846bd5a6fd60c0d3667c874add4fd91f
chj878194 <chj878194@woowahan.com>
2021-12-06 11:22:19 +0900

    feat(build): add build-dest

---
+++ bp-dest/src/App.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import logoImage from 'assets/app-logo.png';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .app-logo {
+    width: 350px;
+    object-fit: cover;
+    margin-bottom: 20px;
+  }
+
+  .project-information {
+    width: 361px;
+  }
+
+  .project-header {
+    font-size: 24px;
+    font-weight: 500;
+  }
+
+  .project-detail {
+    display: flex;
+    flex-direction: row;
+    p,
+    a {
+      font-size: 14px;
+    }
+    p {
+      margin-right: 6px;
+    }
+
+    a,
+    a:link,
+    a:visited {
+      text-decoration: none;
+      color: #3988ff;
+    }
+  }
+`;
+
+const App: React.FC = () => {
+  return (
+    <Container>
+      <img className="app-logo" src={logoImage} />
+      <div className="project-information">
+        <p className="project-header">이제 프로젝트를 시작할 수 있습니다.</p>
+        <div className="project-detail">
+          <p>project setted by</p>
+          <a href="#">@sshrik/bp</a>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default App;
