5a3d85b51cd61c3565a5116abef94664e889fdcd
최현준 <chj878194@woowahan.com>
2022-01-13 23:25:04 +0900

    chore(typo): add i18n setting

---
+++ bp-dest/src/i18n/ko_KR/translation.ts
@@ -0,0 +1,6 @@
+const text = {
+  header: '이제 프로젝트를 시작할 수 있습니다.',
+  info: ['project setted by', '@sshrik/bp'],
+};
+
+export default text;
--- bp-dest/src/index.tsx
+++ bp-dest/src/index.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
-import 'index.css';
+import GlobalStyle, { theme } from './GlobalStyle';
+import 'i18n';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <App />
+  </ThemeProvider>,
+  document.getElementById('root')
+);
